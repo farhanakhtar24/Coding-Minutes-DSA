8ef1d487e6f242d94212c36a86bee657885c7fd7
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2021-11-07 23:33:30 +0530

    cool

--- Essentials/06 Pointer and Dynamic memory/5-pass_by_address.cpp
+++ Essentials/06 Pointer and Dynamic memory/5-pass_by_address.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 //Pass by reference
-void applyTax(int &income)
+void applyTax(int *income)
 {
     float tax = 0.1;
-    income -= (income * tax);
+    *income -= (*income * tax);
 }
 
 int main()
 {
     int income;
     cin >> income;
-    applyTax(income);
+    applyTax(&income);
 
     cout << "Income : " << income << endl;
 
---
+++ Essentials/06 Pointer and Dynamic memory/6-dynamic_memory_allocation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Pass by reference
+void applyTax(int &income)
+{
+}
+
+int main()
+{
+    int *income =
+        return 0;
+}
