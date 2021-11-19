2e4c8ecbb292dcc4ace64c95eee0945a956b714d
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2021-11-19 16:10:11 +0530

    inbuild searching

--- Essentials/07 Vectors/4-inbuilt_searching.cpp
+++ Essentials/07 Vectors/4-inbuilt_searching.cpp
@@ -1,5 +1,13 @@
 /*
 Inbuilt function for doing linear search is find function
+It is a part of a header file known as algorithm
+
+arr.begin() stands for the base address or the address of first element of an array
+arr.end() is an address which stand for the address of that chunk of memory which exist just after the last element
+of the array
+
+the find() function stores the address of the element , whenever it finds the key , but if it doesn't finds the element
+then the iterator stores a the address of arr.end() which is just after the last element of the array
 */
 
 #include <iostream>
@@ -11,7 +19,18 @@ int main()
 {
     vector<int> arr = {10, 10, 2, 11, 31, 2, 3, 4, 5, 6, 7};
 
-    vector<int>::iterator it = find(arr.begin(), arr.end(), );
+    int key;
+    cin >> key;
+    vector<int>::iterator it = find(arr.begin(), arr.end(), key);
 
+    if (it != arr.end())
+    {
+        cout << "Key is present at " << it - arr.begin() << " index" << endl; // to get the index of the iterator we have to
+                                                                              // subtract array's base address from 'it'
+    }
+    else
+    {
+        cout << "Element not found " << endl;
+    }
     return 0;
 }
