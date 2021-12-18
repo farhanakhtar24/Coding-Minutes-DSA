d9320d2e8a99ead7c4b288f37cf0d960bc08ae55
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2021-12-18 16:36:55 +0530

    18-12-20021

--- Essentials/rough_copy.c
+++ Essentials/rough_copy.c
@@ -1,306 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
-    int data;
-    struct node *next;
-} *head = NULL, *p;
+    int a;
+    struct node* next;
+};
+struct node* start = NULL;
 
-int count = 0;
-
-void display()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
+void display(){
+    struct node* cur = (struct node*)malloc(sizeof(struct node*));
+    cur = start;
+    printf("\nElements of Stack.....\n");
+    while(cur != NULL){
+        printf("--> %d\n", cur->a);
+        cur = cur->next;
     }
-    p = head;
-    while (p != NULL)
-    {
-        printf("%d ", p->data);
-        p = p->next;
-    }
-    printf("\n");
 }
 
-void insert_at_begin(int data)
-{
-    count++;
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = data;
-    newnode->next = head;
-    head = newnode;
+void push(){
+    int val;
+    struct node* newnode = (struct node*)malloc(sizeof(struct node*));
+    printf("\nEnter the value : ");
+    scanf("%d", &val);
+    if(start == NULL){
+        newnode->a = val;
+        newnode->next = NULL;
+        start = newnode;
+    }else{
+        newnode->a = val;
+        newnode->next = start;
+        start = newnode;
+    }
     display();
 }
 
-void insert_at_end(int data)
-{
-    count++;
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = data;
-    newnode->next = NULL;
-    p = head;
-    if (p == NULL)
-    {
-        head = newnode;
+void pop(){
+    if(start != NULL){
+        start = start->next;
         display();
-        return;
+    }else{
+        printf("\nThe Stack is empty.....\n");
     }
-    while (p->next != NULL)
-    {
-        p = p->next;
-    }
-    p->next = newnode;
-    display();
 }
 
-void insert_at_pos(int data)
-{
-    int n;
-    printf("Enter position : ");
-    scanf("%d", &n);
-    if (n == 0)
-    {
-        insert_at_begin(data);
-        return;
-    }
-    if (n >= count)
-    {
-        insert_at_end(data);
-        return;
-    }
-    count++;
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = data;
-    p = head;
-    if (p == NULL)
-    {
-        head = newnode;
-        display();
-        return;
-    }
-    for (int i = 1; i < n; i++)
-    {
-        p = p->next;
-    }
-    newnode->next = p->next;
-    p->next = newnode;
-    display();
+void topElement(){
+    if(start != NULL)
+        printf("\nThe top element is %d\n", start->a);
+    else
+        printf("\nThe Stack is empty\n");
 }
 
-void delete_at_begin()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
+void IsEmpty(){
+    if(start == NULL){
+        printf("\nThe Stack is empty.....\n");
+    }else{
+        printf("\nThe Stack is not empty.....\n");
     }
-    p = head;
-    count--;
-    printf("Deleted item = %d .\n", head->data);
-    head = head->next;
-    free(p);
-    display();
 }
 
-void delete_at_end()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
+void nElements(){
+    struct node* cur = (struct node*)malloc(sizeof(struct node*));
+    cur = start;
+    int count = 0;
+    while(cur != NULL){
+        count++;
+        cur = cur->next;
     }
-    p = head;
-    if (p->next == NULL)
-    {
-        delete_at_begin();
-        return;
-    }
-    count--;
-    while (p->next->next != NULL)
-    {
-        p = p->next;
-    }
-    printf("Deleted item = %d .\n", p->next->data);
-    p->next = NULL;
-    free(p->next);
-    display(p->next);
+    printf("\nThe no of Elements in the stack is %d\n", count);
 }
 
-void delete_at_pos()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
-    }
-    int n;
-    printf("Enter position : ");
-    scanf("%d", &n);
-    if (n == 0 || n == 1)
-    {
-        delete_at_begin();
-        return;
-    }
-    if (n >= count)
-    {
-        delete_at_end();
-        return;
-    }
-    count--;
-    p = head;
-    for (int i = 1; i < (n - 1); i++)
-    {
-        p = p->next;
-    }
-    printf("Deleted item = %d .\n", p->next->data);
-    free(p->next);
-    p->next = p->next->next;
-    display();
-}
-
-void total()
-{
-    printf("Total number of elements = %d .\n", count);
-}
-
-void sum_of_elements()
-{
-    int sum = 0;
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
-    }
-    p = head;
-    while (p != NULL)
-    {
-        sum += p->data;
-        p = p->next;
-    }
-    printf("Total sum of elements = %d .\n", sum);
-}
-
-void search_item()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
-    }
-    int data;
-    printf("Enter data to be searched : ");
-    scanf("%d", &data);
-    p = head;
-    int i = 0;
-    while (p != NULL)
-    {
-        i++;
-        if (p->data == data)
-        {
-            printf("Item is present at position = %d .\n", i);
-            return;
-        }
-        p = p->next;
-    }
-    printf("Item is not present. \n");
-}
-
-void max_number()
-{
-    if (head == NULL)
-    {
-        printf("Linked list is empty. \n");
-        return;
-    }
-    int max = head->data;
-    p = head->next;
-    while (p != NULL)
-    {
-        if (max < p->data)
-            max = p->data;
-        p = p->next;
-    }
-    printf(" Maximum number = %d .\n", max);
-}
-
-int main()
+int main(int argc, char const *argv[])
 {
     int ch;
-    printf("------20BEE038------MOHD FARHAN AKHTAR-------\n");
-    while (1)
-    {
-        printf("\n--------------MENU-------------\n");
-        printf("\n1. Insert at beginning.\n");
-        printf("2. Insert at end.\n");
-        printf("3. Insert at given position.\n");
-        printf("4. Delete from beginning.\n");
-        printf("5. Delete from end.\n");
-        printf("6. Delete from given position.\n");
-        printf("7. Total number of elements.\n");
-        printf("8. Sum of elements of linked list.\n");
-        printf("9. Search of item.\n");
-        printf("10. Maximum number in linked list.\n");
-        printf("11. Display .\n");
-        printf("12. Exit. \n");
-    xy:
+    printf("-------20BEE034--------MD SAQUIB SHAKEEL---------");
+     
+
+    do{
+        printf("\n");
+        printf("Options : \n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. IsEmpty\n");
+        printf("4. Top\n");
+        printf("5. No of Elements\n");
+        printf("6. Display\n");
+        printf("7. Exit\n");
+        printf("\n");
         printf("Enter your choice : ");
         scanf("%d", &ch);
-        int num;
-        switch (ch)
-        {
-        case 1:
-            printf("Enter data : ");
-            scanf("%d", &num);
-            insert_at_begin(num);
-            break;
-        case 2:
-            printf("Enter data : ");
-            scanf("%d", &num);
-            insert_at_end(num);
-            break;
-        case 3:
 
-            printf("Enter data : ");
-            scanf("%d", &num);
-            insert_at_pos(num);
-            break;
-        case 4:
-            delete_at_begin();
-            break;
-
-        case 5:
-            delete_at_end();
-            break;
-        case 6:
-            delete_at_pos();
-            break;
-        case 7:
-            total();
-            break;
-        case 8:
-            sum_of_elements();
-            break;
-        case 9:
-            search_item();
-            break;
-        case 10:
-            max_number();
-            break;
-        case 11:
-            display();
-            break;
-        case 12:
-            printf("Exiting ............. \n");
-            return 0;
-        default:
-            printf("Invalid choice, please try again \n");
-            goto xy;
+        switch(ch){
+            case 1: push();
+                break;
+            case 2: pop();
+                break;
+            case 3: IsEmpty();
+                break;
+            case 4: topElement();
+                break;
+            case 5: nElements();
+                break;
+            case 6: display();
+                break;
+            case 7: return 0;
+            default: printf("\nNot a valid option \n");
         }
-    }
-    return 0;
+    }while(1);
 }
