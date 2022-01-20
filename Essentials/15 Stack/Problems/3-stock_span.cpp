3977dd3c4236912d07086827fe9fa219e625e046
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-01-20 13:44:26 +0530

    bcktracking

---
+++ Essentials/15 Stack/Problems/3-stock_span.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void stockSpan(int prices[], int n, int span[])
+{
+    stack<int> s; // indicesof days
+    s.push(0);
+    span[0] = 1;
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+        }
+}
+
+int main()
+{
+
+    return 0;
+}
--- Essentials/rough_copy.cpp
+++ Essentials/rough_copy.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
-#include <stack>
-#include <string>
 
 using namespace std;
-
-int reverse(int n)
+class price_last_year
 {
-}
+public:
+    int price;
+    virtual void setdata(int a = 50)
+    {
+        price = a;
+    }
+    virtual void show(void)
+    {
+        cout << "Updated price is : " << price << endl;
+    }
+};
 
-int main()
+class price_this_year : public price_last_year
 {
-    int n = 467;
-    cout << reverse(n) << endl;
+public:
+    void setdata(int a = 60)
+    {
+        price = a;
+    }
+
+    void show(void)
+    {
+        cout << "Updated price is : " << price << endl;
+    }
+};
+
+int main(void)
+{
+    price_last_year *this_year_ptr = new price_this_year;
+    // price_last_year obj_last;
+    // price_this_year obj_this;
+
+    // this_year_ptr = &obj_this; // a pointer of base class pointing to next year object, but virtual function not working
+
+    this_year_ptr->setdata();
+    this_year_ptr->show();
+
     return 0;
 }
