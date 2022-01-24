328e2b8278e873c132305ee10198d4f118ad8092
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-01-25 00:45:24 +0530

    cmnt added

--- Essentials/16 Queue/intro.cpp
+++ Essentials/16 Queue/intro.cpp
@@ -1,4 +1,8 @@
 /*
 Stack : LIFO => Last In First Out
 Queue : FIFO => First In First Out
+
+Queue adds at the end and pop at the front
+
+Using array we can only implement a circular queue of fixed size
 */
