7fd30bdc2c28837fc4069a8c3bb00f7f3f4b3b9f
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-01-26 18:37:26 +0530

    queue probs

---
+++ Essentials/16 Queue/Problem/stack_using_two_queues.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+// Implement a stack using 2 queues internally as a data structure
+
+class Stack
+{
+    queue<int> q1, q2;
+};
+
+int main()
+{
+
+    return 0;
+}
--- Essentials/16 Queue/intro.cpp
+++ Essentials/16 Queue/intro.cpp
@@ -5,4 +5,38 @@ Queue : FIFO => First In First Out
 Queue adds at the end and pop at the front
 
 Using array we can only implement a circular queue of fixed size
+
+Methods:
+Pop : O(1)
+Push : O(1)
+Front : O(1)
 */
+
+#include <iostream>
+#include "queue.h";
+using namespace std;
+
+int main()
+{
+    Queue nums(7);
+
+    nums.push(1);
+    nums.push(2);
+    nums.push(3);
+    nums.push(4);
+    nums.push(5);
+    nums.push(6);
+    nums.push(7);
+    nums.push(8);
+    nums.pop();
+    nums.pop();
+    nums.push(9);
+
+    while (!nums.empty())
+    {
+        cout << nums.getfront() << " ";
+        nums.pop();
+    }
+
+    return 0;
+}
